2f95dd2b3cf59901fad6f7a7f7421bec6b4f6091
Clara Brandão <102680870+clarabrandaog@users.noreply.github.com>
2025-12-06 21:01:58 -0500

    ITS RESPONSIVE GOD DAMMIT

--- map.js
+++ map.js
@@ -281,27 +281,62 @@ function applyClusterEdgeForces(c) {
 }
 
 // ---------------------- Person class ----------------------
-class Person{
-  constructor(name,picture,bio,fn,department){
-    this.name=name; this.picture=picture||defaultThumb; this.bio=bio; this.fn=fn; this.department=department;
-    this.pos=createVector(random(width),random(height));
-    this.vel=createVector(0,0); this.acc=createVector(0,0);
-    this.size=40; this.radius=this.size/2;
+class Person {
+  constructor(name, picture, bio, fn, department) {
+    this.name = name;
+    this.picture = picture || defaultThumb;
+    this.bio = bio;
+    this.fn = fn;
+    this.department = department;
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+
+    // Set size based on screen width
+    if (windowWidth < 700) {
+      this.size = 40;
+    } else {
+      this.size = 90;
+    }
+    this.radius = this.size / 2;
   }
-  display(){
-    push(); translate(this.pos.x,this.pos.y);
-    drawingContext.save(); drawingContext.beginPath();
-    drawingContext.arc(0,0,this.radius,0,TWO_PI); drawingContext.clip();
+
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+
+    // Clip to circle
+    drawingContext.save();
+    drawingContext.beginPath();
+    drawingContext.arc(0, 0, this.radius, 0, TWO_PI);
+    drawingContext.clip();
+
     imageMode(CENTER);
-    let img=imgCache[this.picture];
-    if(img) image(img,0,0,this.size,this.size);
-    else{fill(150); noStroke(); ellipse(0,0,this.size,this.size);}
+    const img = imgCache[this.picture];
+    if (img) image(img, 0, 0, this.size, this.size);
+    else { fill(150); noStroke(); ellipse(0, 0, this.size, this.size); }
+
     drawingContext.restore();
-    stroke(255); strokeWeight(2); noFill();
-    drawingContext.beginPath(); drawingContext.arc(0,0,this.radius,0,TWO_PI); drawingContext.stroke();
+
+    // Circle border
+    stroke(255);
+    strokeWeight(2);
+    noFill();
+    drawingContext.beginPath();
+    drawingContext.arc(0, 0, this.radius, 0, TWO_PI);
+    drawingContext.stroke();
+
     pop();
   }
+
+  // Optional: call this on window resize to update size
+  updateSize() {
+    if (windowWidth < 700) this.size = 40;
+    else this.size = 90;
+    this.radius = this.size / 2;
+  }
 }
+
 
 function mousePressed(){
   // Check if clicked on any person ball
@@ -315,19 +350,24 @@ function mousePressed(){
   }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-  for(let dept in clusters){
-    const cl=clusters[dept];
-    cl.cx = width/2 + random(-100,100);
-    cl.cy = height/2 + random(-100,100);
-    for(let p of cl.members){
-      let a=random(TWO_PI); let rr=cl.r*sqrt(random());
-      p.pos=createVector(cl.cx+rr*cos(a),cl.cy+rr*sin(a));
-      p.vel=createVector(0,0);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  for (let dept in clusters) {
+    const cl = clusters[dept];
+    cl.cx = width / 2 + random(-100, 100);
+    cl.cy = height / 2 + random(-100, 100);
+    for (let p of cl.members) {
+      let a = random(TWO_PI);
+      let rr = cl.r * sqrt(random());
+      p.pos = createVector(cl.cx + rr * cos(a), cl.cy + rr * sin(a));
+      p.vel = createVector(0, 0);
+
+      // Update size dynamically
+      p.updateSize();
     }
   }
 }
+
 
 // ------------- background spiral --------------
 function drawBackgroundSpiral(){
